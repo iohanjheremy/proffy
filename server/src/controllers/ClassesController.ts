69c0ff62c5d39a3fa2fec2c5c8df9af04a9d7f2d
Iohan Jheremy <iohanjheremy11@gmail.com>
2025-03-13 15:52:17 -0400

    Finalização da API, criação da função de filtragem, correções de Querys e adição do cors.

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from 'express';
+import { Request, Response } from 'express';
 
 import db from '../database/connection';
 import convertHourToMinutes from '../utils/convertHourToMinutes';
@@ -11,69 +11,90 @@ interface ScheduleItem {
 
 export default class ClassesController {
 
-    async index(request: Request, resopnse: Response) {
+    async index(request: Request, response: Response) {
         const filters = request.query;
 
-        if(!filters.week_day || !filters.subject || !filters.time){
-           return response.status(400).json({
-               error: 'Missing filters to search classes' 
-           })
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        if (!filters.week_day || !filters.subject || !filters.time) {
+            return response.status(400).json({
+                error: 'Missing filters to search classes'
+            });
+        }
+    
+        const timeInMinutes = convertHourToMinutes(time);
+        
+        const classes = await db('classes')
+        .whereExists(function() {
+            this.select('classe_schedule.*')
+            .from('classe_schedule')
+            .whereRaw('`classe_schedule` . `class_id` = `classes`. `id`')
+            .whereRaw('`classe_schedule` . `week_day` = ??', [Number(week_day)])
+            .whereRaw('`classe_schedule` . `from` <= ??', [timeInMinutes])
+            .whereRaw('`classe_schedule` . `to` > ??', [timeInMinutes])
+
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id', '=', 'user_id')
+        .select(['classes. *', 'users.*']);
+
+        return response.json(classes);
     }
 
-    const timeInMinutes = convertHourToMinutes(filters.time as string);
-}
 
     async create(request: Request, response: Response) {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-    const trx = await db.transaction();
-
-    try {
-        const insertedUsersIds = await trx('users').insert({
+        const {
             name,
             avatar,
             whatsapp,
             bio,
-        });
-
-        const user_id = insertedUsersIds[0];
-
-        const insertedClassesIds = await trx('classes').insert({
             subject,
             cost,
-            user_id,
-        });
+            schedule
+        } = request.body;
 
-        const class_id = insertedClassesIds[0];
+        const trx = await db.transaction();
 
-        const class_schedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to),
-            }
-        })
+        try {
+            const insertedUsersIds = await trx('users').insert({
+                name,
+                avatar,
+                whatsapp,
+                bio,
+            });
 
-        await trx('classe_schedule').insert(class_schedule);
+            const user_id = insertedUsersIds[0];
 
-        await trx.commit();
+            const insertedClassesIds = await trx('classes').insert({
+                subject,
+                cost,
+                user_id,
+            });
 
-        return response.status(201).send();
-    } catch (err) {
-        await trx.rollback();
+            const class_id = insertedClassesIds[0];
 
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        })
+            const class_schedule = schedule.map((scheduleItem: ScheduleItem) => {
+                return {
+                    class_id,
+                    week_day: scheduleItem.week_day,
+                    from: convertHourToMinutes(scheduleItem.from),
+                    to: convertHourToMinutes(scheduleItem.to),
+                }
+            })
+
+            await trx('classe_schedule').insert(class_schedule);
+
+            await trx.commit();
+
+            return response.status(201).send();
+        } catch (err) {
+            await trx.rollback();
+
+            return response.status(400).json({
+                error: 'Unexpected error while creating new class'
+            })
+        }
     }
-}
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World" });
+});
+
+app.listen(3333);
+
+
 
 // GET: Buscar ou listar uma informação
 // POST: Criar alguma nova informação
@@ -14,10 +25,3 @@ app.use(routes);
 // Corpo (Request Body): Dados para criação ou atualização de um registro
 // Route Params: Identificar qual recurso eu quero atualizar ou deletar
 // Query Params: Paginação, filtros, ordenação
-
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" });
-});
-
-app.listen(3333);
